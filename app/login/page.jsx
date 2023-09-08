b1e0bcf7538eb70df301b203f0feb192bff55bb6
Allen Labrague <[allenlabrague06@gmail.com]>
2023-09-08 21:46:50 +0800

    first commit

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,16 +4,6 @@ import Image from "next/image";
 import { signIn } from "next-auth/react";
 
 const Home = () => {
-  // async function handleGoogleSignin() {
-  //   signIn("google", { callbackUrl: "http://localhost:3000" });
-  // }
-
-  async function handleGoogleSignin() {
-    signIn("google", {
-      callbackUrl: "https://twitter-next-js-sepia.vercel.app/",
-    });
-  }
-
   return (
     <nav className="fixed bottom-0 flex items-center justify-around w-full bg-white z-10 dark:bg-[#121212]">
       <div className="relative w-full h-screen">
@@ -25,7 +15,7 @@ const Home = () => {
         />
         <button
           type="button"
-          onClick={handleGoogleSignin}
+          onClick={() => signIn("google", { callbackUrl: "/" })}
           className="absolute bottom-24 z-10 left-0 right-0 mx-auto w-[90%] bg-white rounded-xl p-5 flex items-center justify-between shadow-xl dark:bg-[#2C2C2C]"
         >
           <h2 className="text-black dark:text-white text-sm">
