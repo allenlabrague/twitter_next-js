ea2b7d7cd1d462056f4699913131989b109ad075
Allen Labrague <[allenlabrague06@gmail.com]>
2023-09-08 22:34:21 +0800

    first commit

--- middleware.jsx
+++ middleware.jsx
@@ -6,7 +6,6 @@ export { default } from "next-auth/middleware";
 // Ref: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
   matcher: [
-    "/",
     "/search",
     "/profile",
     "/update-tweet",
