09d180b1ffe54ca09bc4ba502d1c933ad88d4b22
Allen Labrague <[allenlabrague06@gmail.com]>
2023-09-08 22:55:32 +0800

    first commit

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,30 +1,32 @@
+"use client";
+
 import Image from "next/image";
 import { signIn } from "next-auth/react";
 
 const Home = () => {
   return (
-    <>
-      <nav className="fixed bottom-0 flex items-center justify-around w-full bg-white z-10 dark:bg-[#121212]">
-        <div className="relative w-full h-screen">
-          <Image
-            src="/bgThreads.svg"
-            fill
-            alt="threads"
-            className="absolute top-0 bottom-0 left-0 right-0 mx-auto"
-          />
-          <button
-            type="button"
-            onClick={() => signIn()}
-            className="absolute bottom-24 z-10 left-0 right-0 mx-auto w-[90%] bg-white rounded-xl p-5 flex items-center justify-between shadow-xl dark:bg-[#2C2C2C]"
-          >
-            <h2 className="text-black dark:text-white text-sm">
-              Continue with Google
-            </h2>
-            <Image src="/google.svg" width={30} height={30} alt="home-icon" />
-          </button>
-        </div>
-      </nav>
-    </>
+    <nav className="fixed bottom-0 flex items-center justify-around w-full bg-white z-10 dark:bg-[#121212]">
+      <div className="relative w-full h-screen">
+        <Image
+          src="/bgThreads.svg"
+          fill
+          alt="threads"
+          className="absolute top-0 bottom-0 left-0 right-0 mx-auto"
+        />
+        <button
+          type="button"
+          onClick={() =>
+            signIn("google", { callbackUrl: "http://localhost:3000" })
+          }
+          className="absolute bottom-24 z-10 left-0 right-0 mx-auto w-[90%] bg-white rounded-xl p-5 flex items-center justify-between shadow-xl dark:bg-[#2C2C2C]"
+        >
+          <h2 className="text-black dark:text-white text-sm">
+            Continue with Google
+          </h2>
+          <Image src="/google.svg" width={30} height={30} alt="home-icon" />
+        </button>
+      </div>
+    </nav>
   );
 };
 
